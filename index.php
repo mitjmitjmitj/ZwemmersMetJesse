1d6b5a7bf3ced982558adb5f13eddb9509558c02
Joey Schmitz <4plus7@gmail.com>
2018-11-15 14:27:52 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+
+<?php
+
+$teams = array(34,27,13,98,45,23,11);
+
+foreach ($teams as $team) {
+    $aantal = floor($team / 5);
+    for ($i = 0 ; $i < $aantal ; $i++) {
+        echo '+';
+    }
+    echo '<br>';
+}
+
+
+
+
+?>
+
+
+</div>
+
+</body>
+</html>
